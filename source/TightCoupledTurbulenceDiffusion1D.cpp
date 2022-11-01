81ec9a70e7c25002ad3c8a371f0efc171a2f7441
ethrelfall <77395015+ethrelfall@users.noreply.github.com>
2022-11-01 17:42:45 +0000

    Update TightCoupledTurbulenceDiffusion1D.cpp

--- source/TightCoupledTurbulenceDiffusion1D.cpp
+++ source/TightCoupledTurbulenceDiffusion1D.cpp
@@ -6,7 +6,7 @@
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    std::cout << "Two-way coupled Lorenz model and diffusion model.\n";
 
     //read parameters
     double rho, sigma, beta;  //Lorenz model parameters
